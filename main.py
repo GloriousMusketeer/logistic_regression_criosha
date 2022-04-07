52d9cf44abdb9f918818192c128b63fe279f2f1d
GloriousMusketeer <I.S.efrain.orozco@gmail.com>
2022-04-07 11:21:28 -0500

    agregando label al main

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ def main() -> int:
     li = load_iris()
     x = li.data[:, :2]
     y = (li.target != 0) * 1
-    lf.logistic(x, y)
+    x_index = 0
+    y_index = 1
+    x_label = li.feature_names[x_index]
+    y_label = li.feature_names[y_index]
+    lf.logistic(x, y, x_label, y_label)
     return 0
 
 
