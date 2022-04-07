2c78151b8fd6bc97fbf5bf413847dd92ef9a2504
GloriousMusketeer <I.S.efrain.orozco@gmail.com>
2022-04-07 00:41:29 -0500

    agregamos la función que casi me derrota XD no daba para utilizarla, pero al final vi la luz y todo fue surgiendo

---
+++ lib/sigmoid.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from numpy import array
+from numpy import exp
+
+
+class Sigmoid:
+    def sigmoid_function(self, x: list[float], ) -> list[float]:
+        if x >= 0:
+            sig = 1 / (1 + exp(-x))
+        else:
+            """
+            si x es menor que cero, entonces z será pequeño,
+            el denominador no puede ser cero porque es 1+z. 
+            """
+            sig = exp(x) / (1 + exp(x))
+        return sig
+
+    def sigmoid(self, x):
+        sig = array([self.sigmoid_function(value) for value in x])
+        return sig
+
